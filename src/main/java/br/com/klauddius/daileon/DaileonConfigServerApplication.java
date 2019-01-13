6ff609b4789e7d38fb663c50a4b90e7c7782e8e6
Claudio Brito Moreno <klauddius@gmail.com>
2019-01-13 13:26:57 -0200

    Initial commit

---
+++ src/main/java/br/com/klauddius/daileon/DaileonConfigServerApplication.java
@@ -0,0 +1,14 @@
+package br.com.klauddius.daileon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaileonConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaileonConfigServerApplication.class, args);
+	}
+
+}
+
