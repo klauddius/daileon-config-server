8f2d98b63626f827502145de7666729f6e37fe49
Claudio Brito Moreno <klauddius@gmail.com>
2019-01-13 21:55:18 -0200

    Inclui as configurações para o registro dos serviços.

--- src/main/java/br/com/klauddius/daileon/DaileonConfigServerApplication.java
+++ src/main/java/br/com/klauddius/daileon/DaileonConfigServerApplication.java
@@ -2,13 +2,15 @@ package br.com.klauddius.daileon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class DaileonConfigServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DaileonConfigServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DaileonConfigServerApplication.class, args);
+    }
 
 }
 
